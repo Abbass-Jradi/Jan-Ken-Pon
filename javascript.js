19cd5c6c3c7acd443ec745da4bf92f7fc43d4d16
Abbass_Jradi <jradiabbass@gmail.com>
2022-08-07 17:09:07 +0300

    Add the function to get player's choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-!D
+var result = "";
+
+function getPlayerChoice(){
+    const player_choices = document.querySelectorAll('.player_choice');
+    player_choices.forEach(player_choice => player_choice.addEventListener('click', (e)=>{
+        result = e.target.id;  
+    }))
+}
+
+getPlayerChoice();
