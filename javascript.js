acba526f8b975091ebe9ef9b77659ceedccbafcb
Abbass_Jradi <jradiabbass@gmail.com>
2022-08-07 18:57:48 +0300

    Add Play Again button

--- javascript.js
+++ javascript.js
@@ -26,6 +26,7 @@ function winner(){
     const winner = document.querySelector('#winner');
     const your_score = document.querySelector('#player_score');
     const computer_score = document.querySelector('#ai_score');
+    const final_winner = document.querySelector('#final_winner');
     if((player === 'rock' && computer === 'scissors') ||
        (player === 'paper' && computer === 'rock') ||
        (player === 'scissors' && computer === 'paper'))
@@ -40,15 +41,23 @@ function winner(){
         ai_score += 1;
         computer_score.innerHTML = ai_score;
     }
-    
+    if(round === 5){
+        final_winner.style.opacity='1';
+        if(player_score > ai_score){final_winner.innerHTML = 'You Won!'}
+        else if(ai_score > player_score){final_winner.innerHTML = 'You Lost!'}
+        else{final_winner.innerHTML = 'It is a draw.'}
+    }
 }
 
 function roundCount(){
     const round_count = document.querySelector('#round_count');
-    if(round === 5){
-        round = 0;
-    }
     round +=1;
+    if(round === 5){
+        const button = document.createElement('button');
+        const left = document.querySelector('.left');
+        button.textContent = 'Play Again?';
+        left.appendChild(button);
+    }
     round_count.innerHTML = round;
 }
 
